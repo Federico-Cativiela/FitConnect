c6e6d031c3f6b6eba64d387f2848bced6f7fcc0b
Federico-Cativiela <108641770+Federico-Cativiela@users.noreply.github.com>
2023-07-13 09:02:36 -0300

    fotos de actividades y extras -Fede

--- Back/src/controllers/controllersMembership/postMembershipController.js
+++ Back/src/controllers/controllersMembership/postMembershipController.js
@@ -1,14 +1,12 @@
 const {prisma} = require('../../db.js')
 
-const postMembershipController = async (levelMembership,price,duration ,user,paystore ) => {
+const postMembershipController = async (levelMembership,price,duration  ) => {
     
     const newMembership = await prisma.membership.create({
         data:{
             levelMembership: levelMembership,
             price: price,
             duration: duration,
-            user: user,
-            paystore: paystore,
         }
     })
     return newMembership;
