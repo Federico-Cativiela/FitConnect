01b8810c90c512e5c991aa8e915c40dbf33c1904
Federico-Cativiela <108641770+Federico-Cativiela@users.noreply.github.com>
2023-07-14 12:16:42 -0300

    getMembershipId-Fede

--- Back/src/routes/membership.routes.js
+++ Back/src/routes/membership.routes.js
@@ -1,11 +1,13 @@
 const { Router } = require ('express');
 const { postMembership } = require('../handlers/handlersMembership/postMembershipHandler');
 const { getMembership } = require('../handlers/handlersMembership/getMembershipHandler');
+const {getMembershipId} = require("../controllers/controllersMembership/getMembershipIdController");
 const { putMembership } = require('../controllers/controllersMembership/putMembershipController');
 const { deleteMembership } = require('../controllers/controllersMembership/deleteMembershipController')
 const membershipRouter = Router();
 
 membershipRouter.get("/", getMembership)
+membershipRouter.get("/membership/get/:idMembership", getMembershipId)
 membershipRouter.post("/", postMembership);
 membershipRouter.put("/membership/put/:idMembership", putMembership)
 membershipRouter.delete("/membership/:idMembership", deleteMembership)
