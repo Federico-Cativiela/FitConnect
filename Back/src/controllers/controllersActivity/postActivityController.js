bed72c34f9221bb2445df38db1912c79154a42c4
Federico-Cativiela <108641770+Federico-Cativiela@users.noreply.github.com>
2023-07-15 20:00:11 -0300

    cambiodbActivityyExtraImagesFede

--- Back/src/controllers/controllersActivity/postActivityController.js
+++ Back/src/controllers/controllersActivity/postActivityController.js
@@ -1,9 +1,10 @@
 const {prisma} = require('../../db.js')
 
-const postActivityController = async (name, schedule, type_activity, rating) => {
-    
+const postActivityController = async (image,name, schedule, type_activity, rating) => {
+    console.log(image);
     const newActivity = await prisma.activity.create({
         data:{
+            image: image,
             name: name,
             schedule: schedule,
             type_activity: type_activity,
