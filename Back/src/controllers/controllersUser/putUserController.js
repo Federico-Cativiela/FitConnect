906bde99058b47614fbaaa821b6b97e0b0426e1f
agusbelli <bellisonzia@gmail.com>
2023-07-16 11:31:36 -0300

    userFinished

--- Back/src/controllers/controllersUser/putUserController.js
+++ Back/src/controllers/controllersUser/putUserController.js
@@ -1,12 +1,22 @@
 const { prisma } = require("../../db");
 
 const putUserController = async (id, data) => {
-  const put = await prisma.user.update({
-    where: {
-      idUser: Number(id),
-    },
-    data: data,
-  });
+  let userFound;
+    if (isNaN(id)) {
+      const put = await prisma.user.update({
+        where: {
+          uid: id,
+        },
+        data: data,
+      });
+    }else{
+      const put = await prisma.user.update({
+        where: {
+          id: Number(id)
+        },
+        data: data,
+      });
+    }
 
   return put;
 };
