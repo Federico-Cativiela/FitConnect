f503b45fee43588b2b70d1b3785ef82dffc7324f
agusbelli <bellisonzia@gmail.com>
2023-07-19 13:24:24 -0300

    PutPaystore

--- Back/src/routes/paystore.routes.js
+++ Back/src/routes/paystore.routes.js
@@ -2,13 +2,13 @@ const { Router } = require ('express');
 const { postPaystore } = require("../handlers/handlersPaystore/postPaystoreHandler");
 const { getPaystore } = require('../controllers/controllersPaystore/getPaystoreController');
 // const {getPaystoreId} = require("../controllers/controllersMembership/getMembershipIdController");
-// const { putPaystore } = require('../controllers/controllersMembership/putMembershipController');
+const { putPaystore } = require('../handlers/handlersPaystore/putPaystoreHandler');
 const paystoreRouter = Router();
 
 paystoreRouter.get("/", getPaystore)
 // paystoreRouter.get("/paystore/get/:idPayment", getPaystoreId)
 paystoreRouter.post("/", postPaystore);
-// paystoreRouter.put("/paystore/put/:idPayment", putPaystore)
+paystoreRouter.put("/paystore/:id", putPaystore)
 
 
 module.exports = paystoreRouter;
