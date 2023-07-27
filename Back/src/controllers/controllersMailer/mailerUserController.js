23b5e5644e9d8afd010ebbf5cc6df58ba2971a2f
Federico-Cativiela <108641770+Federico-Cativiela@users.noreply.github.com>
2023-07-27 04:04:42 -0300

    nodemailer6

--- Back/src/controllers/controllersMailer/mailerUserController.js
+++ Back/src/controllers/controllersMailer/mailerUserController.js
@@ -2,11 +2,17 @@ const transporter = require ("../../nodemailer/mailer")
 const nodemailer = require("nodemailer")
 
 const postMailerSucces = async function(newPayment,paymentInfo){
+   const invoiceEmail = newPayment.invoice
     const email = await transporter.sendMail({
         from: "infofitconnect@gmail.com",
         to:paymentInfo.customer_email,
         subject:"Tu pago fue recibido exitosamente ,atentamente FitConnect",
-        body:newPayment.invoice
+        html:`
+        <b>Gracias por confiar en nosotros disfruta tu suscripcion!:</b>
+        <a href="${invoiceEmail}">Consulta tu comprobante aqui!!</a>`
+        
+        
+        
     })
     return email
     
