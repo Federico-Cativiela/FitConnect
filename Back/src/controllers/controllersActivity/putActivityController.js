7448d09e91aad3df04f04de228e059f7f1bbb164
Manuel Rodriguez Sabella <manursabella@gmail.com>
2023-07-27 00:43:42 -0300

    cambio manu

--- Back/src/controllers/controllersActivity/putActivityController.js
+++ Back/src/controllers/controllersActivity/putActivityController.js
@@ -1,21 +1,22 @@
-const { prisma } = require('../../db.js');
+const { prisma } = require("../../db.js");
 
 const putActivitie = async (req, res) => {
+  console.log(req.body, "hola aca estoy");
   try {
-    const put = await prisma.activity.update(
-      {
-        where: {
-          idAct: parseInt(req.params.idAct)
-        },
-        data: req.body
-      })
+    const put = await prisma.activity.update({
+      where: {
+        idAct: parseInt(req.params.idAct),
+      },
 
-    res.json(put)
+      data: req.body,
+    });
+
+    res.json(put);
   } catch (error) {
     console.log(error);
   }
 };
 
 module.exports = {
-  putActivitie
+  putActivitie,
 };
